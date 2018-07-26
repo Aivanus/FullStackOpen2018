6b6e083272ecf60b93276772bbb2745a4d1fd35c
Aivanus <ivank1995@hotmail.com>
2018-07-26 16:44:04 +0300

    2.12 tehty

---
+++ Osa_2/maainfo/src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+const CountryList = ({ filter, countries }) => {
+  if (countries === null) {
+    return null
+  }
+
+  const filtered = countries.filter(
+    c => c.name.toLowerCase().startsWith(filter.toLowerCase())
+  )
+
+  if (filtered.length > 10) {
+    return (
+      <p> Too many results, please be more specific </p>
+    )
+  }
+
+  if(filtered.length === 1){
+    const focused = filtered[0]
+    return(
+      <div>
+        <h1>{focused.name}</h1>
+        <p>Capital: {focused.capital}</p>
+        <p>Population: {focused.population}</p>
+        <img src={focused.flag} alt={focused.name + " flag"} style={{width: 300, height: 150}}/>
+      </div>
+    )
+  }
+
+  return (
+    <ul>
+      {filtered.map(c => <li key={c.alpha3Code}>{c.name}</li>)}
+    </ul>
+  )
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      countries: [],
+      filter: ''
+    }
+  }
+
+  componentDidMount() {
+    console.log('mount')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        this.setState({ countries: response.data })
+      })
+  }
+
+  handleSearchChange = (event) => {
+    this.setState({ filter: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        Find countries: <input value={this.filter} onChange={this.handleSearchChange} />
+        <CountryList filter={this.state.filter} countries={this.state.countries} />
+      </div>
+    )
+  }
+}
+
+export default App;
