c7e77c4a06d21c3f974db6fac75da1bd32071ebb
Aivanus <ivank1995@hotmail.com>
2018-07-26 18:22:38 +0300

    t 2.13 tehty

--- Osa_2/maainfo/src/App.js
+++ Osa_2/maainfo/src/App.js
@@ -1,38 +1,47 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 
-const CountryList = ({ filter, countries }) => {
-  if (countries === null) {
-    return null
-  }
+class CountryList extends React.Component {
+  render() {
+    const { filter, countries, updateParent } = this.props
+    if (countries === null) {
+      return null
+    }
 
-  const filtered = countries.filter(
-    c => c.name.toLowerCase().startsWith(filter.toLowerCase())
-  )
+    const filtered = countries.filter(
+      c => c.name.toLowerCase().startsWith(filter.toLowerCase())
+    )
 
-  if (filtered.length > 10) {
+    if (filtered.length > 10) {
+      return (
+        <p> Too many results, please be more specific </p>
+      )
+    }
+
+    if (filtered.length === 1) {
+      const focused = filtered[0]
+      return (
+        <div>
+          <h1>{focused.name}</h1>
+          <p>Capital: {focused.capital}</p>
+          <p>Population: {focused.population}</p>
+          <img src={focused.flag} alt={focused.name + " flag"} style={{ width: 300, height: 150 }} />
+        </div>
+      )
+    }
+
     return (
-      <p> Too many results, please be more specific </p>
+      <ul>
+        {filtered.map(c =>
+          <li key={c.alpha3Code}>
+            <div onClick={updateParent(c.name)}>
+              {c.name}
+            </div>
+          </li>)
+        }
+      </ul>
     )
   }
-
-  if(filtered.length === 1){
-    const focused = filtered[0]
-    return(
-      <div>
-        <h1>{focused.name}</h1>
-        <p>Capital: {focused.capital}</p>
-        <p>Population: {focused.population}</p>
-        <img src={focused.flag} alt={focused.name + " flag"} style={{width: 300, height: 150}}/>
-      </div>
-    )
-  }
-
-  return (
-    <ul>
-      {filtered.map(c => <li key={c.alpha3Code}>{c.name}</li>)}
-    </ul>
-  )
 }
 
 class App extends Component {
@@ -57,11 +66,15 @@ class App extends Component {
     this.setState({ filter: event.target.value })
   }
 
+  updateFilterFromChild = (value) => {
+    return () => this.setState({ filter: value })
+  }
+
   render() {
     return (
       <div>
         Find countries: <input value={this.filter} onChange={this.handleSearchChange} />
-        <CountryList filter={this.state.filter} countries={this.state.countries} />
+        <CountryList filter={this.state.filter} countries={this.state.countries} updateParent={this.updateFilterFromChild}/>
       </div>
     )
   }
